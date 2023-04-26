3612b8e2e7e46ab0620784470532eb207674af00
ross-wen <124378861+ross-wen@users.noreply.github.com>
2023-04-26 10:45:08 -0400

    Update Q2RandomRange.py

--- Q2RandomRange.py
+++ Q2RandomRange.py
@@ -1,8 +1,2 @@
 # Create a program that accepts 2 numbers from the user. 
 # Your program will output a random number between the range given by the user.
-import math
-
-a = int(input("Input side a:\n"))
-b = int(input("Input side b:\n"))
-c = math.sqrt(a**2 + b**2)
-print("The hypotenuse is:", c)

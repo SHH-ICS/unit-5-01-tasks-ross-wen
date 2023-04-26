a59a16bc45985e077010ab6924efe2f89b7c5fac
ross-wen <124378861+ross-wen@users.noreply.github.com>
2023-04-26 10:45:40 -0400

    Update Q2RandomRange.py

--- Q2RandomRange.py
+++ Q2RandomRange.py
@@ -1,2 +1,7 @@
 # Create a program that accepts 2 numbers from the user. 
 # Your program will output a random number between the range given by the user.
+import random
+num1 = int(input("Input a number: \n"))
+num2 = int(input("Input a second number: \n"))
+random = random.randint(num1,num2)
+print(random)
